e0510679f6f51bb4f50743137cb234ff5f917d85
f4t1h4rsl4n <artemis1940@gmail.com>
2020-03-30 14:35:57 -0400

    framework first v

---
+++ src/test/java/qa/automation/utilities/BrowserUtils.java
@@ -0,0 +1,36 @@
+package qa.automation.utilities;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BrowserUtils {
+
+    /**
+     * Pause test for some time
+     * Don't use if you don't need it. It's using Thread.sleep
+     * @param seconds
+     */
+    public static void wait(int seconds) {
+        try {
+            Thread.sleep(1000 * seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * waits for backgrounds processes on the browser to complete
+     *
+     * @param timeOutInSeconds
+     */
+    public static void waitForPageToLoad(long timeOutInSeconds) {
+        ExpectedCondition<Boolean> expectation = driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+        try {
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeOutInSeconds);
+            wait.until(expectation);
+        } catch (Throwable error) {
+            error.printStackTrace();
+        }
+    }
+}
