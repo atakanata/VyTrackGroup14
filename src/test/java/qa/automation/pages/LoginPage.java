f8037682b1dbd3fe21cded01b1b8a67898c1e2d2
f4t1h4rsl4n <artemis1940@gmail.com>
2020-03-30 14:47:00 -0400

    LoginPage added

---
+++ src/test/java/qa/automation/pages/LoginPage.java
@@ -0,0 +1,60 @@
+package qa.automation.pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import qa.automation.utilities.BrowserUtils;
+import qa.automation.utilities.ConfigurationReader;
+import qa.automation.utilities.Driver;
+
+public class LoginPage {
+
+    @FindBy(id = "prependedInput")
+    private WebElement username;
+
+    @FindBy(id = "prependedInput2")
+    private WebElement password;
+
+    @FindBy(id = "_submit")
+    private WebElement login;
+
+    @FindBy(linkText = "Forgot your password?")
+    private WebElement forgotPassword;
+
+    @FindBy(css = "[class='alert alert-error']")
+    private WebElement warningMessage;
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public String getWarningMessageText() {
+        return warningMessage.getText();
+    }
+
+    /**
+     * Method to login, version #1
+     * Login as a specific user
+     *  This method can be usefull when properties has more passwords
+     * @param usernameValue
+     * @param passwordValue
+     */
+    public void login(String usernameValue, String passwordValue) {
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+        BrowserUtils.waitForPageToLoad(10);
+        BrowserUtils.wait(3);
+    }
+
+    /**
+     * Method to login, version #2
+     * User credentials will be retrieved from configuration.properties file
+     */
+    public void login(String user) {
+        username.sendKeys(ConfigurationReader.getProperty(user));
+        password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
+        BrowserUtils.waitForPageToLoad(10);
+        BrowserUtils.wait(3);
+    }
+}
