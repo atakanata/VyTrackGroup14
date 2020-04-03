13dcc99c35eebd4f421ed00973d5c6669c64c9b0
sekercelik <sceliksidika@gmail.com>
2020-04-03 11:31:11 -0400

    added US2AC1 Test

--- src/test/java/qa/automation/pages/VehiclesStoreSalesManagerPage.java
+++ src/test/java/qa/automation/pages/VehiclesStoreSalesManagerPage.java
@@ -10,9 +10,80 @@ public class VehiclesStoreSalesManagerPage extends AbstractPageBase{
     @FindBy(className= "grid-header-row")
     public WebElement allVehiclesHeader;
 
-    public String getAllVehiclesHeaderInfo(){
-        return allVehiclesHeader.getText();
+    public WebElement getAllVehiclesHeaderInfo(){
+        return allVehiclesHeader;
     }
+
+//*****************************************************************************************************
+
+ //   @FindBy(className = "btn main-group btn-primary pull-right")
+     @FindBy(partialLinkText = "Create Car")
+    public WebElement createCar;
+
+    public WebElement getCreateCarButton(){
+        return createCar;
+    }
+//*****************************************************************************************************
+
+   @FindBy(xpath = "//input[contains(@id,'custom_entity_type_LicensePlate-uid')]")
+    public WebElement licencePlateInputBox;
+
+    public WebElement getLicencePlateInputBox(){
+        return licencePlateInputBox;
+    }
+//*****************************************************************************************************
+//
+    @FindBy(xpath = "//input[contains(@id='custom_entity_type_Tags_0-uid')]")
+    public WebElement tagsJuniorcheckBox;
+
+    public void getFirstTagCheckBox(){
+        tagsJuniorcheckBox.click();
+    }
+
+//*****************************************************************************************************
+
+    @FindBy(xpath = "//input[contains(@id='custom_entity_type_Driver-uid')]")
+    public WebElement driverInputBox;
+
+    public WebElement getDriverInputBox(){
+        return driverInputBox;
+    }
+
+ //*****************************************************************************************************
+    @FindBy(xpath = "//input[contains(@id='custom_entity_type_Location-uid')]")
+    public WebElement locationInputBox;
+
+    public WebElement getLocationInputBox(){
+        return locationInputBox;
+    }
+
+ //*****************************************************************************************************
+
+    @FindBy(xpath = "//input[contains(@id='custom_entity_type_ModelYear-uid')]")
+    public WebElement modelYearInputBox;
+
+    public WebElement getModelYearInputBox(){
+        return modelYearInputBox;
+    }
+//*****************************************************************************************************
+    @FindBy(className = "btn btn-success action-button")
+    public WebElement saveAndCloseButton;
+
+    public void clickSaveAndCloseButton(){
+         saveAndCloseButton.click();
+    }
+
+//*****************************************************************************************************
+
+    @FindBy(className = "flash-messages-frame")
+    public WebElement entitySavedMessage;
+
+    public String getEntitySavedMessage(){
+        return entitySavedMessage.getText();
+    }
+//*****************************************************************************************************
+
+
 
 
 }
