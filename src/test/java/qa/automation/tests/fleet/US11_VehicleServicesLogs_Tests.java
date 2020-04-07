1d3215320a40550c3d89721f0dca4a9fd42babe0
atakanata <atakanata@gmail.com>
2020-04-07 16:32:01 -0400

    US-11 ac1 and ac2 added

--- src/test/java/qa/automation/tests/fleet/US11_VehicleServicesLogs_Tests.java
+++ src/test/java/qa/automation/tests/fleet/US11_VehicleServicesLogs_Tests.java
@@ -5,10 +5,9 @@ import org.testng.annotations.Test;
 import qa.automation.pages.LoginPage;
 import qa.automation.pages.fleet.US11_VehicleServicesLogsPage;
 import qa.automation.tests.AbstractTestBase;
+import qa.automation.utilities.Driver;
 
 public class US11_VehicleServicesLogs_Tests extends AbstractTestBase {
-
-
 
     @Test
     public void ac01Test(){
@@ -18,4 +17,23 @@ public class US11_VehicleServicesLogs_Tests extends AbstractTestBase {
         servicesLogsPage.navigateTo("Fleet", "Vehicle Services Logs");
         Assert.assertEquals(servicesLogsPage.getpageSubtitle(),"VehicleServicesLogs");
     }
+
+    @Test
+    public void ac02aTest(){
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("storemanager1");
+        US11_VehicleServicesLogsPage servicesLogsPage = new US11_VehicleServicesLogsPage();
+        servicesLogsPage.navigateTo("Fleet", "Vehicle Services Logs");
+        Assert.assertNotEquals(Driver.getDriver().getTitle(),"Vehicle Services Logs - Entities - System - Car - Entities - System");
+    }
+
+    @Test
+    public void ac02bTest(){
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("salesmanager1");
+        US11_VehicleServicesLogsPage servicesLogsPage = new US11_VehicleServicesLogsPage();
+        servicesLogsPage.navigateTo("Fleet", "Vehicle Services Logs");
+        Assert.assertNotEquals(Driver.getDriver().getTitle(),"Vehicle Services Logs - Entities - System - Car - Entities - System");
+    }
+
 }
